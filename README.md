e6bb75d6340bbe7e176faca2ba665639fc344011
Patrick Meredor <trickmeredor@gmail.com>
2026-03-25 20:58:06 +0800

    Feat: Put the project in the github

---
+++ README.md
@@ -0,0 +1,5 @@
+What we will do for tonight is: 
+
+1. Create a form for logging in
+2. Check if the email is exising at the users table.
+3. If existing logged the user in and redirect the user at the dashboard
---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Czwmpc | Project</title>
+    <link rel="stylesheet" href="assets/css/style.css">
+    <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
+</head>
+
+<body>
+    <nav class="flex flex-row justify-between items-center mx-auto p-5 ">
+        <h1 class="font-bold text-2xl">Cavite Zone Workers Multipurpose Cooperation</h1>
+
+        <div class="flex flex-row justify-center items-center gap-3">
+            <a href="auth/login/index.php" 
+            class="border px-2 py-2 rounded"
+            >
+                Login
+            </a>
+            <a href="auth/register/index.php" 
+                class="px-2 py-2 rounded bg-green-500 text-black hover:border-green-300 hover:text-white transition-all duration-300"
+                >
+                Register
+            </a>
+        </div>
+    </nav>
+</body>
+
+</html>
